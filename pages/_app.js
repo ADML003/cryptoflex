afb17c1b9ab9f50c6f16a8dc500e2fb7dd432707
Aditya Malhotra <adml003.pro@gmail.com>
2025-02-11 23:58:43 +0530

    nights out

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,24 @@
 import "../styles/globals.css";
+import Script from "next/script";
+import { ThemeProvider } from "next-themes";
+import React from "react";
+// import Navbar from "../components/Navbar";
+// import Footer from "../components/Footer";
+
+import { Navbar, Footer } from "../components";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider attribute="class">
+      <div className="dark:bg-nft-dark bg-white min-h-screen">
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
+      </div>
+      <Script
+        src="https://kit.fontawesome.com/c8f74919dc.js"
+        crossorigin="anonymous"
+      ></Script>
+    </ThemeProvider>
+  );
 }
