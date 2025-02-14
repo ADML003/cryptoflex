f023c2ffbb12b1a7134146ca0c6f6766bc56c3cd
Aditya Malhotra <adml003.pro@gmail.com>
2025-02-14 19:34:32 +0530

    footer partially done

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import Image from "next/image";
+import images from "../assets";
+import Button from "./Button";
+
+const FooterLinks = { heading, items };
 
 const Footer = () => {
   return <div>Footer</div>;
